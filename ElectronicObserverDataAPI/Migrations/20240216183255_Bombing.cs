5f37337c9e9f88972aec9a760be2adcf62b2e9bb
Jebzou <22751386+Jebzou@users.noreply.github.com>
2024-02-16 19:36:34 +0100

    Bombing

---
+++ ElectronicObserverDataAPI/Migrations/20240216183255_Bombing.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectronicObserverDataAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Bombing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Bombing",
+                table: "FitBonusValueModel",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bombing",
+                table: "FitBonusValueModel");
+        }
+    }
+}
